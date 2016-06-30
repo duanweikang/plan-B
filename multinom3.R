f92415d45556412324e814f2167f6feda4deb7d6
duanweikang <danielsduan@gmail.com>
2016-06-29 21:21:55 -0500

    first commit, involves code for model, simulations and test on real data set.

---
+++ multinom3.R
@@ -0,0 +1,223 @@
+### multinomial extension ###
+
+# getpartition data is used to get the initial partition
+# the function will return the number of elements in each subinterval
+
+getpartition = function(m,k,n){
+  #m is the min size of each set in the partition
+  #k is the number of partitons
+  #n is the total number of points to be partitioned
+  dum = 0
+  while(dum<100){
+    foo = c(0,sort(sample(m:(n-m+1),k-1)),n)
+    foo1 = foo[2:(k+1)]-foo[1:k]
+    if(min(foo1) >= m){ 
+      ans = foo1
+      dum = 105
+    }
+    else dum<-dum+1
+  }
+  if(dum>101) {
+    return(ans)
+  }
+  else {
+    return(NULL)
+  }
+}
+
+library(partitions
+        )
+prob = function(v){
+  return(sum(v)/prod(v))
+}
+
+mrprob = function(y){
+  w = apply(y,1,sum)
+  length = dim(y)[2]
+  category = dim(y)[1]
+  count = sum(w==0)
+  if((category-count)==1){
+    return(1)
+  }
+  else{
+    # kth stage
+    k_category = category - count
+    w = w[!w %in% c(0)]
+    comb = compositions(length,k_category,include.zero=F)
+    sums = sum(apply(comb,2,sum))
+    return(prob(w)/sums)
+  }
+}
+
+
+# getszinfo will take the partition result and data as input
+# the function will return two elements
+# the number of 1s in each subinterval for each category
+# the loglikelhood for each subinterval
+
+getszinfo_multinom = function(sz,y,ct){
+  k = length(sz)
+  dd = c(0,cumsum(sz))
+  tysz = matrix(rep(0,ct*k),nrow=ct,ncol=k)
+  lgprb = NULL
+  
+  for(i in 1:k){
+    for(j in 1:ct){
+      tysz[j,i] = sum(y[j,(dd[i]+1):dd[i+1]])
+    }
+  }
+  for(i in 1:k){
+    lgprb = c(lgprb,log(mrprob(y[,(dd[i]+1):dd[i+1]])))
+  }
+  return(list(tysz,lgprb))
+}
+
+# onestep function will propose a change on partition
+# calculate the log likelihood of old and new partition choices
+# and decided to whether to move new parition or stay in old position
+
+onestep_multinom = function(sz,tysz,lgprb,y,m,k,ct){
+  newsz = sz
+  #      newtysz<-tysz
+  #      newlgprb<-lgprb
+  
+  dum = (1:k)[sz>m]
+  lndum = length(dum)
+  if(lndum==1){
+    di = dum
+  }
+  else {
+    di = sample(dum,1)
+  } #we will remove guys from sz[di]
+  # determine the direction of move, 1 for right, -1 for left
+  dn = sample(1:(sz[di]-m),1) #this is the number to be moved
+  if(di==1){
+    dd = 1
+  } 
+  else if(di==k){
+    dd = -1
+  }
+  else{
+    dd = sample(c(-1,1),1) #this is the direction we move them; 1=to right
+  }
+  if(dd==1){ 
+    newsz[c(di,di+1)] = newsz[c(di,di+1)] +c(-dn,dn) 
+  }
+  else{  
+    newsz[c(di-1,di)] = newsz[c(di-1,di)] + c(dn,-dn) 
+  }
+  
+  if(min(newsz)<m){
+    write(c(sz,di,dn,dd,newsz),file="error.txt")
+  }
+  foo = getszinfo_multinom(newsz,y,ct)
+  # we will get the log likelihood of new partition
+  newtysz = foo[[1]]
+  newlgprb = foo[[2]]
+  newdum = (1:k)[newsz>m]
+  lnnewdum = length(newdum)
+  # in the five cases i am finding log(p(o|n)/p(n|o))=lgrt
+  # and decide wether to move or not
+  if(di==1){
+    lgrt = (log((1/lnnewdum)*(1/(newsz[2]-m))*(1/2)) -
+              log((1/lndum)*(1/(sz[1]-m))*1))
+  }
+  else if(di==2 & dd==-1) {
+    lgrt = (log((1/lnnewdum)*(1/(newsz[1]-m))*1) -
+              log((1/lndum)*(1/(sz[2]-m))*(1/2)))
+  }
+  else if(di==k){
+    lgrt = (log((1/lnnewdum)*(1/(newsz[k-1]-m))*(1/2)) -
+              log((1/lndum)*(1/(sz[k]-m))*1))
+  }
+  else if (di==k-1 & dd==1) {
+    lgrt = (log((1/lnnewdum)*(1/(newsz[k]-m))*1) -
+              log((1/lndum)*(1/(sz[k-1]-m))*(1/2)))
+  }
+  else if (di >= 2 & di<= k-2 & dd==1) {
+    lgrt = (log((1/lnnewdum)*(1/(newsz[di+1]-m))*(1/2)) -
+              log((1/lndum)*(1/(sz[di]-m))*(1/2)))
+  }
+  else if (di>=3 & di<=k-1 & dd==-1) {
+    lgrt = (log((1/lnnewdum)*(1/(newsz[di-1]-m))*(1/2)) -
+              log((1/lndum)*(1/(sz[di]-m))*(1/2)))
+  }
+  lgrtall = sum(newlgprb-lgprb) + sum(lgrt)
+  #        return(lgrtall)
+  if(runif(1) <= exp(lgrtall)){
+    return(list(newsz,newtysz,newlgprb))
+  }
+  
+  else return(0)
+}
+
+# manysteps will allow us to construct the markov chain 
+# the markov chain will approximate the posterior distribution of partitions
+# we will get the average of esimate for each observation in each partition as the final estimate
+# the function will return the estimate and the acceptance rate of MCMC
+
+
+# y is the response variable data, notes that it should be ordered based on predictor value
+# m is the minimum number of observations in each subinterval
+# k is the number of partitions in each partition
+# R is the length of MCMC
+# ct is the number of categories
+
+manysteps_multinom = function(y,m,k,R,ct){
+  n = dim(y)[2]
+  sz = getpartition(m,k,n)
+  if(length(sz)<1) return(0)
+  #        cat("sz", sz,"\n")
+  foo = getszinfo_multinom(sz,y,ct)
+  tysz = foo[[1]]
+  lgprb = foo[[2]]
+  est = matrix(rep(0,k*ct),nrow=ct,ncol=k)
+  for(i in 1:k){
+    for(j in 1:ct){
+      est[j,i]=tysz[j,i]/sz[i]
+    }
+  }
+  e = NULL
+  for(i in 1:k){
+    step = matrix(rep(0,ct*sz[i]),nrow=ct,ncol=sz[i])
+    for(j in 1:ct){
+      step[j,] = rep(est[j,i],sz[i])
+    }
+    e = cbind(e,step)
+  }
+  
+  mv = 0
+  ansest = matrix(rep(0,ct*n),nrow=ct,ncol=n)
+  dans = NULL
+  # here we construct the Markov Chain
+  for(i in 1:R){
+    dum = onestep_multinom(sz,tysz,lgprb,y,m,k,ct)
+    dans = c(dans,NULL)
+    if(length(dum)==1) {
+      ansest = ansest + e
+    }
+    else{
+      mv = mv + 1
+      sz = dum[[1]]
+      tysz = dum[[2]]
+      lgprb = dum[[3]]
+      est = matrix(rep(0,k*ct),nrow=ct,ncol=k)
+      for(i in 1:k){
+        for(j in 1:ct){
+          est[j,i]=tysz[j,i]/sz[i]
+        }
+      }
+      e = NULL
+      for(i in 1:k){
+        step = matrix(rep(0,ct*sz[i]),nrow=ct,ncol=sz[i])
+        for(j in 1:ct){
+          step[j,] = rep(est[j,i],sz[i])
+        }
+        e = cbind(e,step)
+      }
+      ansest = ansest + e
+    }
+  }
+  #        cat("% times moved", mv/R,"\n")
+  return(list(mv/R,ansest/R))
+}
---
+++ test_on_data_multi.R
@@ -0,0 +1,123 @@
+############# test on real data ##################
+
+
+##################################################
+############### election data ####################
+##################################################
+
+setwd("/home/weikangduan/Desktop/plan B")
+nes = read.table('nes.txt',header=T)
+library(nnet)
+# head(nes)
+# str(nes)
+
+########## nonparametric estimation ##############
+
+# sort data by income
+attach(nes)
+new_nes = nes[order(income),]
+n = dim(new_nes)[1] 
+ct = dim(new_nes)[2]
+dat = matrix(0,nrow=ct,ncol=n)
+for(i in 1:n){
+  for(j in 1:ct){
+    if(new_nes$pid[i] == 'republican'){
+      dat[1,i] = 1
+    }
+    else if(new_nes$pid[i] == 'democrat'){
+      dat[2,i] = 1
+    }
+    else{
+      dat[3,i] = 1
+    }
+  }
+}
+
+# estimate
+p_np = manysteps_multinom(y=dat,m=5,k=37,R=1e3,ct=3)
+
+############ logistic regression ###############
+mod = multinom(pid~income,data=nes)
+summary(mod)
+p_multi = predict(mod,newdata=data.frame(nes[,3]),type='probs')
+
+
+### merge the probability together for comparison ###
+republican_np = cbind(seq(1,944,1),rep('republican',944),p_np[[2]][1,])
+democrat_np = cbind(seq(1,944,1),rep('democrat',944),p_np[[2]][2,])
+independent_np = cbind(seq(1,944,1),rep('independent',944),p_np[[2]][3,])
+
+# data for nonparametic method
+df_np = rbind(democrat_np,republican_np,independent_np)
+colnames(df_np) = c('income_order','pid','probability')
+df_np = as.data.frame(df_np)
+df_np$income_order = as.numeric(as.character(df_np$income_order))
+df_np$pid = as.factor(df_np$pid)
+df_np$probability = as.numeric(as.character(df_np$probability))
+#str(df_np)
+
+# data for logistic regression
+republican_multi = cbind(seq(1,944,1),rep('republican',944),p_multi[,3])
+democrat_multi = cbind(seq(1,944,1),rep('democrat',944),p_multi[,1])
+independent_multi = cbind(seq(1,944,1),rep('independent',944),p_multi[,2])
+df_multi = rbind(democrat_multi,republican_multi,independent_multi)
+colnames(df_multi) = c('income_order','pid','probability')
+df_multi = as.data.frame(df_multi)
+df_multi$income_order = as.numeric(as.character(df_multi$income_order))
+df_multi$pid = as.factor(df_multi$pid)
+df_multi$probability = as.numeric(as.character(df_multi$probability))
+#str(df_multi)
+
+###plot for np method
+plt_np = ggplot(data=df_np,aes(x=income_order,y=probability))
+plt_np = plt_np+geom_line(aes(color=pid),alpha=1/2,size=2)+scale_color_manual(values=c('#3366ff','#00cc66','#ff3366'))+facet_grid(pid~.)
+plt_np = plt_np+labs(title='Estimation of Probability on Political ID by Nonparametric Model')
+plt_np = plt_np+labs(x='Income_order')+labs(y='Probability')
+plt_np
+
+###plot for nonparametric method
+plt_multi = ggplot(data=df_multi,aes(x=income_order,y=probability))
+plt_multi = plt_multi+geom_line(aes(color=pid),alpha=1/2,size=2)+scale_color_manual(values=c('#3366ff','#00cc66','#ff3366'))+facet_grid(pid~.)
+plt_multi = plt_multi+labs(title='Estimation of Probability on Political ID by Multi logit model')
+plt_multi = plt_multi+labs(x='Income_order')+labs(y='Probability')
+plt_multi
+
+### compare prediction error ###
+pred_y_multi = rep(NA,944)
+for(i in 1:944){
+  if(which.is.max(p_multi[i,])==1){
+    pred_y_multi[i] = 'democrat'
+  } 
+  else if(which.is.max(p_multi[i,])==2){
+    pred_y_multi[i] = 'independent'
+  }
+  else{
+    pred_y_multi[i] = 'republican'
+  }
+}
+
+pred_y_np = rep(NA,944)
+for(i in 1:944){
+  if(which.is.max(p_np[[2]][,i])==1){
+    pred_y_np[i] = 'republican'
+  } 
+  else if(which.is.max(p_np[[2]][,i])==2){
+    pred_y_np[i] = 'democrat'
+  }
+  else{
+    pred_y_np[i] = 'independent'
+  }
+}
+
+# check error
+error_multi = 0
+error_np = 0
+for(i in 1:944){
+  if(new_nes$pid[i] != pred_y_multi[i]){
+    error_multi = error_multi+1
+  }
+  if(new_nes$pid[i] != pred_y_np[i]){
+    error_np = error_np+1
+  }
+}
+
